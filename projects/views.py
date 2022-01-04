b5a0deeed4089fbe9bc1d8c8a21c6a1324cff3bb
Sangkon Han <sigmadream@gmail.com>
2022-01-04 13:30:20 +0900

    init model and crud

---
+++ projects/views.py
@@ -0,0 +1,51 @@
+from django.shortcuts import render, redirect
+from .models import Project, Tag, Review
+from .forms import ProjectForm
+
+
+def projects(request):
+    projects = Project.objects.all()
+    context = {'projects': projects}
+    return render(request, 'projects/projects.html', context)
+
+
+def project(request, pk):
+    project_obj = Project.objects.get(id=pk)
+    context = {'project': project_obj}
+    return render(request, 'projects/project.html', context)
+
+
+def create_project(request):
+    project_form = ProjectForm()
+
+    if request.method == 'POST':
+        project_form = ProjectForm(request.POST)
+        if project_form.is_valid():
+            project_form.save()
+            return redirect('projects')
+
+    context = {'form': project_form}
+    return render(request, 'projects/project_form.html', context)
+
+
+def update_project(request, pk):
+    project = Project.objects.get(id=pk)
+    project_form = ProjectForm(instance=project)
+
+    if request.method == 'POST':
+        project_form = ProjectForm(request.POST, instance=project)
+        if project_form.is_valid():
+            project_form.save()
+            return redirect('projects')
+
+    context = {'form': project_form}
+    return render(request, 'projects/project_form.html', context)
+
+
+def delete_project(request, pk):
+    project = Project.objects.get(id=pk)
+    if request.method == 'POST':
+        project.delete()
+        return redirect('projects')
+    context = {'object': project}
+    return render(request, 'projects/project_delete.html', context)
