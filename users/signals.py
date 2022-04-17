dba3e4b2e5125d425865882e2b78d162aef2c9b9
Sangkon Han <sigmadream@gmail.com>
2022-04-17 21:58:30 +0900

    // wip

--- users/signals.py
+++ users/signals.py
@@ -1,7 +1,8 @@
-from django.contrib.auth.models import User
 from django.db.models.signals import post_save, post_delete
-
-from users.models import Profile
+from django.contrib.auth.models import User
+from .models import Profile
+from django.core.mail import send_mail
+from django.conf import settings
 
 
 def create_profile(sender, instance, created, **kwargs):
@@ -11,14 +12,40 @@ def create_profile(sender, instance, created, **kwargs):
             user=user,
             username=user.username,
             email=user.email,
-            name=user.first_name
+            name=user.first_name,
+        )
+
+        subject = 'Welcome to DevSearch'
+        message = 'We are glad you are here!'
+
+        send_mail(
+            subject,
+            message,
+            settings.EMAIL_HOST_USER,
+            [profile.email],
+            fail_silently=False,
         )
 
 
+def update_user(sender, instance, created, **kwargs):
+    profile = instance
+    user = profile.user
+
+    if not created:
+        user.first_name = profile.name
+        user.username = profile.username
+        user.email = profile.email
+        user.save()
+
+
 def delete_user(sender, instance, **kwargs):
-    user = instance.user
-    user.delete()
+    try:
+        user = instance.user
+        user.delete()
+    except:
+        pass
 
 
 post_save.connect(create_profile, sender=User)
+post_save.connect(update_user, sender=Profile)
 post_delete.connect(delete_user, sender=Profile)
--- users/views.py
+++ users/views.py
@@ -1,21 +1,21 @@
-from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
-from django.contrib import messages
 from django.contrib.auth import login, authenticate, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 from django.contrib.auth.models import User
-from users.models import Profile
-from users.forms import CustomUserCreationForm
+from .models import Profile
+from .forms import CustomUserCreationForm, ProfileForm, SkillForm, MessageForm
+from .utils import search_profiles, paginate_profiles
 
 
-def login_page(request):
-    page = 'login'
-    context = {'page': page}
+def login_user(request):
+    # page = 'login'
 
     if request.user.is_authenticated:
         return redirect('profiles')
 
     if request.method == 'POST':
-        username = request.POST['username']
+        username = request.POST['username'].lower()
         password = request.POST['password']
 
         try:
@@ -27,11 +27,12 @@ def login_page(request):
 
         if user is not None:
             login(request, user)
-            return redirect('profiles')
-        else:
-            messages.error(request, 'Username OR Password is incorrect')
+            return redirect(request.GET['next'] if 'next' in request.GET else 'account')
 
-    return render(request, 'users/login_register.html', context)
+        else:
+            messages.error(request, 'Username OR password is incorrect')
+
+    return render(request, 'users/login_register.html')
 
 
 def logout_user(request):
@@ -51,36 +52,144 @@ def register_user(request):
             user.username = user.username.lower()
             user.save()
 
-            messages.success(request, "User account was created")
+            messages.success(request, 'User account was created!')
 
             login(request, user)
-            return redirect('profiles')
+            return redirect('edit-account')
+
         else:
-            messages.error(request, "An error has occurred during registration")
+            messages.success(
+                request, 'An error has occurred during registration')
 
     context = {'page': page, 'form': form}
     return render(request, 'users/login_register.html', context)
 
 
 def profiles(request):
-    profiles = Profile.objects.all()
-    context = {'profiles': profiles}
+    _profiles, _search_query = search_profiles(request)
+    _custom_range, _profiles = paginate_profiles(request, _profiles, 3)
+    context = {'profiles': _profiles, 'search_query': _search_query, 'custom_range': _custom_range}
     return render(request, 'users/profiles.html', context)
 
 
 def user_profile(request, pk):
-    profile = Profile.objects.get(id=pk)
-
-    top_skills = profile.skill_set.exclude(description__exact="")
-    other_skills = profile.skill_set.filter(description="")
-
-    context = {'profile': profile, 'top_skills': top_skills, 'other_skills': other_skills}
+    _profile = Profile.objects.get(id=pk)
+    _top_skills = _profile.skill_set.exclude(description__exact="")
+    _other_skills = _profile.skill_set.filter(description="")
+    context = {'profile': _profile, 'topSkills': _top_skills, "otherSkills": _other_skills}
     return render(request, 'users/user-profile.html', context)
 
 
 @login_required(login_url='login')
 def user_account(request):
-    profile = request.user.profile
-
-    context = {'profile': profile}
+    _profile = request.user.profile
+    _skills = _profile.skill_set.all()
+    _projects = _profile.project_set.all()
+    context = {'profile': _profile, 'skills': _skills, 'projects': _projects}
     return render(request, 'users/account.html', context)
+
+
+@login_required(login_url='login')
+def edit_account(request):
+    _profile = request.user.profile
+    form = ProfileForm(instance=_profile)
+
+    if request.method == 'POST':
+        form = ProfileForm(request.POST, request.FILES, instance=_profile)
+        if form.is_valid():
+            form.save()
+            return redirect('account')
+    context = {'form': form}
+    return render(request, 'users/profile_form.html', context)
+
+
+@login_required(login_url='login')
+def create_skill(request):
+    _profile = request.user.profile
+    form = SkillForm()
+    if request.method == 'POST':
+        form = SkillForm(request.POST)
+        if form.is_valid():
+            skill = form.save(commit=False)
+            skill.owner = _profile
+            skill.save()
+            messages.success(request, 'Skill was added successfully!')
+            return redirect('account')
+
+    context = {'form': form}
+    return render(request, 'users/skill_form.html', context)
+
+
+@login_required(login_url='login')
+def update_skill(request, pk):
+    _profile = request.user.profile
+    _skill = _profile.skill_set.get(id=pk)
+    form = SkillForm(instance=_skill)
+
+    if request.method == 'POST':
+        form = SkillForm(request.POST, instance=_skill)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Skill was updated successfully!')
+            return redirect('account')
+    context = {'form': form}
+    return render(request, 'users/skill_form.html', context)
+
+
+@login_required(login_url='login')
+def delete_skill(request, pk):
+    _profile = request.user.profile
+    _skill = _profile.skill_set.get(id=pk)
+    if request.method == 'POST':
+        _skill.delete()
+        messages.success(request, 'Skill was deleted successfully!')
+        return redirect('account')
+    context = {'object': _skill}
+    return render(request, 'delete_template.html', context)
+
+
+@login_required(login_url='login')
+def inbox(request):
+    _profile = request.user.profile
+    _message_requests = _profile.messages.all()
+    _unread_count = _message_requests.filter(is_read=False).count()
+    context = {'messageRequests': _message_requests, 'unreadCount': _unread_count}
+    return render(request, 'users/inbox.html', context)
+
+
+@login_required(login_url='login')
+def view_message(request, pk):
+    _profile = request.user.profile
+    _message = _profile.messages.get(id=pk)
+    if not _message.is_read:
+        _message.is_read = True
+        _message.save()
+    context = {'message': _message}
+    return render(request, 'users/message.html', context)
+
+
+def create_message(request, pk):
+    _recipient = Profile.objects.get(id=pk)
+    form = MessageForm()
+    try:
+        sender = request.user.profile
+    except:
+        sender = None
+
+    if request.method == 'POST':
+        form = MessageForm(request.POST)
+        if form.is_valid():
+            message = form.save(commit=False)
+            message.sender = sender
+            message.recipient = _recipient
+
+            if sender:
+                message.name = sender.name
+                message.email = sender.email
+            message.save()
+
+            messages.success(request, 'Your message was successfully sent!')
+            return redirect('user-profile', pk=_recipient.id)
+
+    context = {'recipient': _recipient, 'form': form}
+    return render(request, 'users/message_form.html', context)
